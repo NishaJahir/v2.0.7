b62fb02f9fa3f5969eea918efc95c93530165937
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-07-12 16:27:00 +0530

    Update novalnet_paypal.js

--- resources/js/novalnet_paypal.js
+++ resources/js/novalnet_paypal.js
@@ -1,36 +1,28 @@
-jQuery(document).ready( function() {
+var $ = jQuery.noConflict();
+
+$(document).ready( function() {
 	
-	jQuery('#savepayment').click(function() {
-        if (!jQuery('#savepayment').is(':checked')) {
-            notSavePaymentData();
+	$('#savepayment').click(function() {
+        if (!$('#savepayment').is(':checked')) {
+            $('#save_payment').val('');
         } else {
-            savePaymentData();
+            $('#save_payment').val('1');
         }
 	});
 	
-	function savePaymentData()
-	{
-	jQuery('#save_payment').val('1');
-	}
-
-	function notSavePaymentData()
-	{
-	jQuery('#save_payment').val('');
-	}
-	
-	jQuery('#nn_toggle_form').on('click',function(){
-		if (jQuery('#nn_new_card_details').css('display') == 'none'){
-			jQuery('#nn_new_card_details').show();
-			jQuery('#save_payment_block').show();
-			jQuery('#nn_saved_details').hide();
-			jQuery('#nn_new_details').val('1');
-			jQuery('#nn_toggle_form').html(jQuery('#nn_account_display_text_saved').val());
+	$('#nn_toggle_form').on('click',function(){
+		if ($('#nn_new_card_details').css('display') == 'none'){
+			$('#nn_new_card_details').show();
+			$('#save_payment_block').show();
+			$('#nn_saved_details').hide();
+			$('#nn_new_details').val('1');
+			$('#nn_toggle_form').html($('#nn_account_display_text_saved').val());
 		}else{
-			jQuery('#nn_new_card_details').hide();
-			jQuery('#save_payment_block').hide();
-			jQuery('#nn_saved_details').show();
-			jQuery('#nn_new_details').val('0');
-			jQuery('#nn_toggle_form').html(jQuery('#nn_account_display_text_new').val());
+			$('#nn_new_card_details').hide();
+			$('#save_payment_block').hide();
+			$('#nn_saved_details').show();
+			$('#nn_new_details').val('0');
+			$('#nn_toggle_form').html($('#nn_account_display_text_new').val());
 		}
 			
 	});
